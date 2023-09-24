999b543b315b2adbd6a7ba0cb2e9dd4146b17b74
Mahmoud swilam <123952365+swilam202@users.noreply.github.com>
2023-09-24 15:48:25 +0300

    create remote data source

--- lib/movies/data/data source/movie remote data source.dart
+++ lib/movies/data/data source/movie remote data source.dart
@@ -1,4 +1,54 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:moviemagnet/core/error/exceptions.dart';
+import 'package:moviemagnet/core/error/failures.dart';
+import 'package:moviemagnet/core/network/api%20constants.dart';
+import 'package:moviemagnet/movies/data/models/movie%20model.dart';
 
-abstract class BaseMovieReomteDataSource{
-  
+abstract class BaseMovieReomteDataSource {
+  Future<Either<Failure, List<MovieModel>>> getNowPlayingMovies();
+  Future<Either<Failure, List<MovieModel>>> getPopularMovies();
+  Future<Either<Failure, List<MovieModel>>> getTopRatedMovies();
+}
+
+class MovieReomteDataSource extends BaseMovieReomteDataSource {
+  Dio dio = Dio();
+  @override
+  Future<Either<Failure, List<MovieModel>>> getNowPlayingMovies() async {
+    Response response = await dio.get(APIConstants.nowPlayingMoviesLink);
+    List<MovieModel> movies = response.data['results']
+        .map((element) => MovieModel.fromJson(element))
+        .toList();
+    if (response.statusCode == 200) {
+      return Right(movies);
+    } else {
+      throw Left(ServerException(response.data));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<MovieModel>>> getPopularMovies() async {
+    Response response = await dio.get(APIConstants.popularMoviesLink);
+    List<MovieModel> movies = response.data['results']
+        .map((element) => MovieModel.fromJson(element))
+        .toList();
+    if (response.statusCode == 200) {
+      return Right(movies);
+    } else {
+      throw Left(ServerException(response.data));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<MovieModel>>> getTopRatedMovies() async {
+    Response response = await dio.get(APIConstants.topRatedMoviesLink);
+    List<MovieModel> movies = response.data['results']
+        .map((element) => MovieModel.fromJson(element))
+        .toList();
+    if (response.statusCode == 200) {
+      return Right(movies);
+    } else {
+      throw Left(ServerException(response.data));
+    }
+  }
 }
