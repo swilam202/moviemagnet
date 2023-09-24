d123e0127e2fd879810903d94908ae02b74802b4
Mahmoud swilam <123952365+swilam202@users.noreply.github.com>
2023-09-24 13:13:31 +0300

    create movie model in data source layer

---
+++ lib/movies/data/models/movie model.dart
@@ -0,0 +1,26 @@
+
+import 'package:moviemagnet/movies/domain/entites/movie.dart';
+
+class MovieModel extends Movie {
+  MovieModel({
+    required super.id,
+    required super.backdropPath,
+    required super.ganderIds,
+    required super.overview,
+    required super.title,
+    required super.voteAvg,
+    required super.releaseDate,
+  });
+
+  factory MovieModel.fromJson(Map<String,dynamic> json) {
+    return MovieModel(
+      id: json['id'],
+      backdropPath: json['backdrop_path'],
+      ganderIds: json['genre_ids'],
+      overview: json['overview'],
+      title: json['title'],
+      voteAvg: json['vote_average'],
+      releaseDate: json['release_date'],
+    );
+  }
+}
