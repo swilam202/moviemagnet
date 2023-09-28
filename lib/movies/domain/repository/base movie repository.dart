19814c6dfaeaf71445f94efef8f4ac94de92775c
Mahmoud swilam <123952365+swilam202@users.noreply.github.com>
2023-09-28 17:23:45 +0300

    create domain repo and usecase

--- lib/movies/domain/repository/base movie repository.dart
+++ lib/movies/domain/repository/base movie repository.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:moviemagnet/movies/domain/entites/movie%20details.dart';
 
 import '../../../core/error/failures.dart';
 import '../../data/models/movie model.dart';
@@ -8,5 +9,6 @@ abstract class BaseMovieRepository{
     Future<Either<Failure, List<MovieModel>>> getPopularMovies();
     Future<Either<Failure, List<MovieModel>>> getTopRatedMovies();
     Future<Either<Failure, List<MovieModel>>> getUpComingMovies();
+    Future<Either<Failure, MovieDetails>> getMovieDetails();
     
 }
---
+++ lib/movies/domain/usecases/get movie detils usecase.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:moviemagnet/movies/domain/entites/movie%20details.dart';
+import 'package:moviemagnet/movies/domain/repository/base%20movie%20repository.dart';
+
+import '../../../core/error/failures.dart';
+
+class GetMovieDetailsUseCasw {
+  BaseMovieRepository baseMovieRepository;
+  GetMovieDetailsUseCasw(this.baseMovieRepository);
+  Future<Either<Failure, MovieDetails>> excute() async {
+    return await baseMovieRepository.getMovieDetails();
+  }
+}
